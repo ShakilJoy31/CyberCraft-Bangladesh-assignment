46692b2830356c3d8bb97a2224b6857a5710d66c
ShakilJoy31 <joy31a@gmail.com>
2025-03-16 19:23:35 +0600

    16 march

--- lib/baseURL.tsx
+++ lib/baseURL.tsx
@@ -1,4 +1,5 @@
-const productionURL = 'http://localhost:5000';
+// const productionURL = 'http://localhost:5000';
+const productionURL = 'https://cybercraft-bangladesh-assignment-server-1.onrender.com'
 export const getBaseURL = () => {
     return productionURL;
 }
